42bf3272dfc6b9d2f739dca9a94122e2c60fc825
Polytimos <precieux.nsiemo@ynov.com>
2020-06-01 15:27:13 +0200

    HomeSlide

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -4,16 +4,12 @@ import './SideDrawer.css'
 
 const SideDrawer = props => (
     <nav className="side-drawer">
+        <div className="homeSlide"><div className="homePSlide1">Connectez-vous</div> <div className="homeSlide2"> Inscrivez-vous </div></div>
         <ul>
             <li><a href="/">Connexion</a></li>
             <li><a href="/">Paniers</a></li>
             <li><a href="/">Parfums</a></li>
-            <li><a href="/">XXXXX</a></li>
-            <li><a href="/">XXXXX</a></li>
-            <li><a href="/">XXXXX</a></li>
-            <li><a href="/">XXXXX</a></li>
-            <li><a href="/">XXXXX</a></li>
-            <li><a href="/">XXXXX</a></li>
+
         </ul>
     </nav>
 );
